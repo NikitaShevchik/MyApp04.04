2358f4b74aefa9e8af47bda23216b2971143ed09
NikitaShevchik <shevchik.nikita@gmail.com>
2022-08-04 18:26:56 +0300

    onCLick

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
 function App() {
-  function getDigitsSum(a) {
-    let sum = 0;
-    for (let i = 0; i < String(a).length; i++) {
-      sum = sum + Number(String(a)[i]);
-    }
-    return sum;
+  function alertF(num) {
+    alert(num)
   }
 
   return <div>
-    {getDigitsSum(12345)}
-  </div>
+    <button onClick={() => alertF(1)}>act1</button>
+    <button onClick={() => alertF(2)}>act2</button>
+    <button onClick={() => alertF(3)}>act3</button>
+  </div>;
 }
+
+
+// Навешивание событий в JSX
+
 // Вспомогательные функции 
 
 // Тернарный оператор ternary operator
