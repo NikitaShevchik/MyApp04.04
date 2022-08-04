922b44a7643643f4794413111c8efa9d5ca2d5a3
NikitaShevchik <shevchik.nikita@gmail.com>
2022-08-04 14:45:32 +0300

    firstpagePractice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+    Another text
+    <p style={{ backgroundColor: "red" }}>Первый абзац</p>
+    <p style={{ backgroundColor: "yellow" }}>Второй абзац</p>
+    <p style={{ backgroundColor: "teal" }}>Третий абзац</p>
+    <p>Четвертый абзац</p>
+    <input type="text" /><br /><input type="text" /><br /><input type="text" />
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+    </ul>
+    <table style={{ fontSize: "42px" }}>
+      <tbody>
+        <tr>
+          <td>Jakob</td>
+          <td>Andre</td>
+          <td>Paul</td>
+        </tr>
+        <tr>
+          <td>Semio</td>
+          <td>Arer</td>
+          <td>Kipo</td>
+        </tr>
+        <tr>
+          <td>Alig</td>
+          <td>Afvce</td>
+          <td>Jfia</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 }
 
 export default App;
+
+
+
+// Задача 3.1
+// Поменяйте текст внутри дива. Посмотрите на изменения, произошедшие в браузере.
+// Задача 3.2
+// Добавьте в див несколько абзацев.
+// Задача 3.3
+// Добавьте в див несколько инпутов, разделенных тегами br.
+// Задача 3.4
+// Сделайте внутри дива список ul, содержащий в себе 10 тегов li.
+// Задача 3.5
+// Сделайте внутри дива таблицу с тремя рядами и тремя колонками.
+// Задача 3.6
+// Сделайте внутри дива три абзаца с различными CSS классами.
